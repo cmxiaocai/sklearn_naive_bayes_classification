f1922be633def4092e6a76240911798b5df7c9c5
thsheep <thsheep@live.com>
2018-04-18 23:44:17 +0800

    update

--- sklearn_naive_bayes_classification/贝叶斯分类器.py
+++ sklearn_naive_bayes_classification/贝叶斯分类器.py
@@ -12,20 +12,32 @@ from sklearn.externals import joblib
 
 
 
-# 加载分类器
-clf = joblib.load('model.pkl')
-# 加载矢量
+def foo():
+    clf = joblib.load('model.pkl')
+    count_vect = joblib.load('count_vect.pkl')
+    testing_data = load_files('./predict_test', encoding='utf-8')
+    target_names = json.loads(open('training_data.target', 'r', encoding='utf-8').read())
+    #     # 字符串处理
 
-count_vect = joblib.load('count_vect.pkl')
-testing_data = load_files('./predict_test', encoding='utf-8')
-target_names = json.loads(open('training_data.target', 'r', encoding='utf-8').read())
-#     # 字符串处理
+    tfidf_transformer = TfidfTransformer()
+    # 将文本集合转为矩阵
+    X_new_counts = count_vect.transform(testing_data.data)
+    X_new_tfidf = tfidf_transformer.fit_transform(X_new_counts)
+    # 进行预测
+    predicted = clf.predict(X_new_tfidf)
+    for title, category in zip(testing_data.filenames, predicted):
+        print('%r => %s' % (title, target_names[category]))
 
-tfidf_transformer = TfidfTransformer()
-# 将文本集合转为矩阵
-X_new_counts = count_vect.transform(testing_data.data)
-X_new_tfidf = tfidf_transformer.fit_transform(X_new_counts)
-# 进行预测
-predicted = clf.predict(X_new_tfidf)
-for title, category in zip(testing_data.filenames, predicted):
-    print('%r => %s' % (title, target_names[category]))
+
+def foo2():
+    testing_data = load_files('./predict_test', encoding='utf-8')
+    clf = joblib.load('./pipeline_model.pkl')
+    target_names = json.loads(open('pipeline_training_data.target', 'r', encoding='utf-8').read())
+    clf.predict(testing_data.data)
+    predicted = clf.predict(testing_data.data)
+    for title, category in zip(testing_data.filenames, predicted):
+        print('%r => %s' % (title, target_names[category]))
+
+
+if __name__ == '__main__':
+    foo2()
